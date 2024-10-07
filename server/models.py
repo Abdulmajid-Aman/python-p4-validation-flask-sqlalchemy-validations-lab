d32c277c60a62d2805ade892f4a988184f6ece99
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-10-07 20:19:28 +0300

    "Added validators for Author and Post models to enforce data constraints on name, phone number, content, summary, category, and title fields."

--- server/models.py
+++ server/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 db = SQLAlchemy()
+import re
 
 class Author(db.Model):
     __tablename__ = 'authors'
@@ -12,9 +13,22 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('name')
+    def validate_name(self, key, name):
+        authors = [author.name for author in Author.query.all()]
+        if not name or name in authors:
+            raise ValueError('Author must have a name')
+        return name
+        
+    @validates("phone_number")
+    def validate_phone_number(self, key, value):
+        if not re.match(r"^\d{10}$", value):
+            raise ValueError("Phone number must be 10 digits")
+        return value
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
+    
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -28,6 +42,31 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
+    @validates("content")
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError("Title entered is an invalid option")
+        return content
+
+    @validates("summary")
+    def validate_summary(self, key, value):
+        if len(value) > 250:
+            raise ValueError("Title entered is an invalid option")
+        return value
+
+    @validates("category")
+    def validate_category(self, key, value):
+        category = ['Fiction', 'Non-Fiction']
+        if value not in category:
+            raise ValueError('Category entered is an invalid choice')
+        return value
+    
+    @validates('title')
+    def validate_title(self, key, value):
+        titles = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(title in value for title in titles):
+            raise ValueError('Title must contain one of the keywords: "Won\'t Believe", "Secret", "Top", "Guess"')
+        return value
 
 
     def __repr__(self):
